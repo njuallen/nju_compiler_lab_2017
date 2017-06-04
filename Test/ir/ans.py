400e9a2dbbf5713496bfdef9b99f93180e10fb71
Allen <njuallen@foxmail.com>
2017-06-04 20:53:09 +0800

    Added report and handouts for lab3.

--- Test/ir/ans.py
+++
@@ -1,7 +0,0 @@
-with open("in", "r") as f:
-    s = f.read();
-    l = s.split(",")
-    count = 0;
-    for a in l:
-        print "ans[%d] = %s;" % (count, a.split()[0]) 
-        count = count + 1
